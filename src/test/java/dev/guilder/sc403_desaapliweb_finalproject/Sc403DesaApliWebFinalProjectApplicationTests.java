610f981306805745a84772c2d27f74677fb9cfb4
Guilder W. Milliner <guilderw@gmail.com>
2022-02-15 15:57:50 -0600

    Initial commit

---
+++ src/test/java/dev/guilder/sc403_desaapliweb_finalproject/Sc403DesaApliWebFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilder.sc403_desaapliweb_finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sc403DesaApliWebFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
