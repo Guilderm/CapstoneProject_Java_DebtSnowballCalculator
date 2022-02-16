5285b12201c2fbb8f42ae8b537ef718fe538fe7e
Guilder W. Milliner <guilderw@gmail.com>
2022-02-16 10:13:03 -0600

    Restructuring pages
    Set home as default page

--- src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/PublicController.java
+++ src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/PublicController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class PublicController {
     @GetMapping
     public String Home() {
-        return "index";
+        return "public/home";
     }
 }
