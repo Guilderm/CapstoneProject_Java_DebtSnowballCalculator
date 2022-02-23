862ebce22034ad0ef1af8f7d47906f033f13f8da
Guilder W. Milliner <guilderw@gmail.com>
2022-02-22 21:23:51 -0600

    Restructuring pages

--- src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/Public.java
+++ src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/Public.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PublicController {
-    @GetMapping
+public class Public {
+    @GetMapping("/")
     public String Home() {
         return "public/home";
+    }
+
+    @GetMapping("/pricing")
+    public String contactPage() {
+        return "public/pricing";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage() {
+        return "public/about";
     }
 }
