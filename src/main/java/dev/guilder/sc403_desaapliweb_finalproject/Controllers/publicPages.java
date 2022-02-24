e5380d01148bb36d8e95f791ab421e9f91242aea
Guilder W. Milliner <guilderw@gmail.com>
2022-02-24 01:31:18 -0600

    Created the bases of all pages

--- src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/publicPages.java
+++ src/main/java/dev/guilder/sc403_desaapliweb_finalproject/Controllers/publicPages.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Public {
+public class publicPages {
     @GetMapping("/")
     public String Home() {
         return "public/home";
