f1a16924a75ac4a8977eba850138209cc62396e8
Angelo Sotomayor <assv.9330@gmail.com>
2022-03-03 22:55:07 -0600

    Entity, Repository and Services packages are created

---
+++ src/main/java/dev/guilder/SnowballCalculator/Services/UserService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package dev.guilder.SnowballCalculator.Services;
+
+/**
+ *
+ * @author assv9
+ */
+public interface UserService {
+    
+}
