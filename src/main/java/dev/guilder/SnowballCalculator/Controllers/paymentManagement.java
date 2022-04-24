733a61947f325d354536d2090b5e925c69076e94
Angelo Sotomayor <assv.9330@gmail.com>
2022-04-23 22:36:42 -0600

    Update paymentManagement.java

--- src/main/java/dev/guilder/SnowballCalculator/Controllers/paymentManagement.java
+++ src/main/java/dev/guilder/SnowballCalculator/Controllers/paymentManagement.java
@@ -1,10 +1,19 @@
 package dev.guilder.SnowballCalculator.Controllers;
 
+import dev.guilder.SnowballCalculator.Entitys.Product;
+import dev.guilder.SnowballCalculator.Services.ProductService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class paymentManagement {
+    @Autowired
+    private ProductService productService;
+    
     @GetMapping("/Checkout")
     public String checkoutPage() {
         return "paymentManagement/checkoutPage";
@@ -13,5 +22,13 @@ public class paymentManagement {
     @GetMapping("/invoice")
     public String invoicePage() {
         return "paymentManagement/invoicePage";
+    }
+    
+    @GetMapping("/addCart/{productId}")
+    public String addCart(@PathVariable("productId") Long idProduct, Model model){
+        List<Product> productList = productService.getAllProducts();
+        //Product product = productService.getProductById(idProduct);
+        model.addAttribute("newItem", productList);
+        return "redirect:/shoppingCart";
     }
 }
