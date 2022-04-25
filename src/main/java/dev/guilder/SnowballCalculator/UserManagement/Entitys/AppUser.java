c0a91054a91b5091418d07c47fbfe508b4dd971a
Guilder W. Milliner <guilderw@gmail.com>
2022-04-25 01:00:36 -0600

    Working on login
    Removed TMP login page

--- src/main/java/dev/guilder/SnowballCalculator/UserManagement/Entitys/AppUser.java
+++ src/main/java/dev/guilder/SnowballCalculator/UserManagement/Entitys/AppUser.java
@@ -73,10 +73,6 @@ public class AppUser implements UserDetails {
         return firstName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
     @Override
     public boolean isAccountNonExpired() {
         return true;
